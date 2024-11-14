b74514d75138f6bbf2f0f8e1a671689360487ce6
taninpong <thaninpong@perfenterprise.com>
2024-11-14 13:14:36 +0700

    update bot

--- demoChatBot/demoChatBot/Dialogs/MainDialog.cs
+++ demoChatBot/demoChatBot/Dialogs/MainDialog.cs
@@ -4,17 +4,9 @@ using Microsoft.Bot.Schema;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Collections.Generic;
-using Microsoft.Bot.Builder.Dialogs.Choices;
 using System;
-using System.Linq;
 using demoChatBot.Models;
-using Newtonsoft.Json.Linq;
 using demoChatBot.Services;
-using Microsoft.AspNetCore.DataProtection.XmlEncryption;
-using Flurl.Http;
-using System.Net.Http;
-using MongoDB.Driver.Core.Configuration;
-using demoChatBot;
 
 namespace demoChatBot.Dialogs
 {
@@ -88,7 +80,6 @@ namespace demoChatBot.Dialogs
             var attachments = new List<Attachment>();
             var reply = MessageFactory.Attachment(attachments);
             var data = stepContext.Context.Activity.Text;
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"restaurantDetails:{restaurantDetails}{Environment.NewLine}  restaurantDetails.StatusRestaurant:{restaurantDetails.StatusRestaurant}"));
             if (data?.ToLower() == "reset")
             {
                 restaurantDetails.IsLinkedAccount = false;
@@ -122,16 +113,9 @@ namespace demoChatBot.Dialogs
                             }
                             else
                             {
-                                var messageText = $"สถานะร้าน ปิด";
-                                var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
-                                return await stepContext.PromptAsync(nameof(ChoicePrompt), new PromptOptions
-                                {
-                                    Prompt = promptMessage,
-                                    Choices = new[]
-                                    {
-                                    new Choice { Value = "เปิดร้าน" }
-                                }
-                                }, cancellationToken);
+
+                                await stepContext.Context.SendActivityAsync(MessageFactory.Text($"สถานะร้าน ปิด"));
+                                return await stepContext.EndDialogAsync(null, cancellationToken);
                             }
                     }
                 }
